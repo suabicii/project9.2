63cb71e623dd504d788749b8b33347c387a926e3
Michal Slabik <michal.slabik3@wp.pl>
2017-04-29 00:02:31 +0200

    Add js/scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = [];
+allNames = allNames.concat(femaleNames,maleNames);
+var newName = 'Marian';
+
+if (allNames.indexOf(newName) === -1) {
+	allNames = allNames.push(newName);
+};
